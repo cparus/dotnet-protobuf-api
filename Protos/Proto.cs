8fd6d18ff1437b0ce498d861de35da9295f6718c
Cam <cam@Cams-Mac-Pro.local>
2019-02-12 11:10:16 -0600

    ..

--- Protos/Proto.cs
+++ Protos/Proto.cs
@@ -5,7 +5,7 @@ using System;
 namespace dotnet_protobuf_api.Protos
 {
     [ProtoContract]
-    public class ProtoModel
+    public class JabroniModel
     {
         [ProtoMember(1)]
         public int Id { get; set; }
